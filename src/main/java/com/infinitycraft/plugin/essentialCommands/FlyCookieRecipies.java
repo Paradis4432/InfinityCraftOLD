4979022413b5028f1631928b80bb5fe2001098ec
LucasUser <Lucasezequiel120202@gmail.com>
2021-04-02 15:07:02 -0300

    Initial Commit

---
+++ src/main/java/com/infinitycraft/plugin/essentialCommands/FlyCookieRecipies.java
@@ -0,0 +1,9 @@
+package com.infinitycraft.plugin.essentialCommands;
+
+import org.bukkit.inventory.ShapedRecipe;
+
+public class FlyCookieRecipies {
+    /**
+     * ON PAUSE UNTIL WE FINISH CUSTOM DROPS AND SEE HOW LONG IT WOULD TAKE TO CRAFT EACH COOKIE
+     */
+}
